80e602db52cd1399dcd871a2c5be6833e2a2d26b
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-08 16:28:20 +0200

    [MT-4469] Rename `BookmarkNodeLayout` to `BookmarkCardLayout`

--- src/format/elements/BookmarkNode.ts
+++ src/format/elements/BookmarkNode.ts
@@ -5,7 +5,7 @@ export const BookmarkNode = {
     TYPE: 'bookmark',
 };
 
-export enum BookmarkNodeLayout {
+export enum BookmarkCardLayout {
     VERTICAL = 'vertical',
     HORIZONTAL = 'horizontal',
 }
@@ -14,7 +14,7 @@ export interface BookmarkNode extends Element<typeof BookmarkNode.TYPE> {
     uuid: string;
     url: string;
     oembed: OEmbedInfo;
-    layout: BookmarkNodeLayout;
+    layout: BookmarkCardLayout;
     show_preview_image: boolean;
     open_in_new_tab: boolean;
 }
