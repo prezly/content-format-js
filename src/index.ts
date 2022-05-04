ab1a94dedd50bd4108666bf9ff692c23b808eadd
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-04 17:54:22 +0300

    Allow deep-imports of EmailDocument, NoteDocument, StoryDocument

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ export * from '@prezly/uploads';
 export * from './format';
 export * from './traits';
 
-import * as Email from './EmailContent';
-import * as Note from './NoteContent';
-import * as Story from './StoryContent';
+import * as Email from './EmailDocument';
+import * as Note from './NoteDocument';
+import * as Story from './StoryDocument';
 
 export type EmailPlaceholderType = Email.PlaceholderType;
-export type EmailDocument = Email.Document;
+export type EmailContent = Email.Document;
 export const EmailContent = {
     validate: Email.validate,
 };
 
-export type NoteDocument = Note.Document;
-export const NoteDocument = {
+export type NoteContent = Note.Document;
+export const NoteContent = {
     validate: Note.validate,
 };
 
 export type StoryPlaceholderType = Story.PlaceholderType;
-export type StoryDocument = Story.Document;
-export const StoryDocument = {
+export type StoryContent = Story.Document;
+export const StoryContent = {
     validate: Story.validate,
 };
