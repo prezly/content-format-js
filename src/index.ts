374c92178846091b59962617a395bdd745752f64
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-04 15:30:54 +0300

    Export all traits interfaces publicly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from '@prezly/uploads';
 export * from './format';
-export type { Alignment, CardLayout } from './traits';
+export * from './traits';
 export * from './NoteContent';
 export * from './EmailContent';
 export * from './NoteContent';
