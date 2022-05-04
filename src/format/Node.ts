d78ca3c4968e15a94ffea35d18614b12e68ed967
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-04 16:41:38 +0300

    Add `ComposedElement` to the `Node` union type

--- src/format/Node.ts
+++ src/format/Node.ts
@@ -1,7 +1,8 @@
 import { type Element, isElement } from './Element';
 import { type Text, isText } from './Text';
+import type { ComposedElement } from './ComposedElement';
 
-export type Node = Element | Text;
+export type Node = ComposedElement | Element | Text;
 
 export function isNode(value: any): value is Node {
     return isText(value) || isElement(value);
