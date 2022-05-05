78aafe7c8fda0ee75ffd0831e37971b39cfa40e4
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-05 17:20:34 +0300

    Fix failing build

--- packages/note-content-format/src/index.ts
+++ packages/note-content-format/src/index.ts
@@ -1,5 +1,5 @@
 export { Alignment, CardLayout } from '@prezly/content-format';
-export { UploadedFile, UploadedImage, isUploadedFile, isUploadedImage } from '@prezly/content-format';
+export { type UploadedFile, type UploadedImage, isUploadedFile, isUploadedImage } from '@prezly/content-format';
 
 export * from './types';
 export * from './type-guards';
