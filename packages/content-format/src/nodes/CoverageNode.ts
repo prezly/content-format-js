f0db65dfe022a517f6f6df9cd730836952ccd221
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-13 14:54:03 +0300

    [MT-4622] Update `CoverageNode` with new attributes

--- packages/content-format/src/nodes/CoverageNode.ts
+++ packages/content-format/src/nodes/CoverageNode.ts
@@ -1,13 +1,20 @@
 import { Element } from '../Element';
-import { isNonZeroInteger, isObject, isUuid } from '../validation';
+import {isBoolean, isEnum, isNonZeroInteger, isObject, isUuid} from '../validation';
 
 export interface CoverageNode extends Element<typeof CoverageNode.TYPE> {
     uuid: string;
     coverage: { id: number };
+    layout: CoverageNode.Layout;
+    new_tab: boolean;
+    show_thumbnail: boolean;
 }
 
 export namespace CoverageNode {
     export const TYPE = 'coverage';
+    export enum Layout {
+        VERTICAL = 'vertical',
+        HORIZONTAL = 'horizontal',
+    }
 
     export function isCoverageNode(value: any): value is CoverageNode {
         return Element.isElement(value, TYPE);
@@ -17,6 +24,9 @@ export namespace CoverageNode {
         const isValid =
             isCoverageNode(value) &&
             isUuid(value.uuid) &&
+            isEnum(value.layout, Layout) &&
+            isBoolean(value.new_tab) &&
+            isBoolean(value.show_thumbnail) &&
             isObject(value.coverage) &&
             isNonZeroInteger(value.coverage.id);
 
