6a440f3d1221ccd8d7a68d88d4e290067d0a85e1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-09-20 14:17:50 +0300

    [DEV-8850] Add BC support for `placeholder` nodes

--- packages/content-format/src/nodes/VariableNode.ts
+++ packages/content-format/src/nodes/VariableNode.ts
@@ -1,17 +1,25 @@
 import { Element } from '../Element';
 import { isNonEmptyString } from '../validation';
 
-export interface VariableNode<Key extends string> extends Element<typeof VariableNode.TYPE> {
+export interface VariableNode<Key extends string>
+    extends Element<typeof VariableNode.TYPE | typeof VariableNode.BACKWARD_COMPATIBLE_TYPE> {
     key: `${Key}`;
 }
 
 export namespace VariableNode {
     export const TYPE = 'variable';
+    /**
+     * @deprecated
+     */
+    export const BACKWARD_COMPATIBLE_TYPE = 'placeholder';
 
     export function isVariableNode<Variable extends VariableNode<Key>, Key extends string = string>(
         value: any,
     ): value is Variable {
-        return Element.isElement(value, VariableNode.TYPE);
+        return (
+            Element.isElement(value, VariableNode.TYPE) ||
+            Element.isElement(value, VariableNode.BACKWARD_COMPATIBLE_TYPE)
+        );
     }
 
     export function validateVariableNode<Placeholder extends VariableNode<string>>(value: any): Placeholder | null;
